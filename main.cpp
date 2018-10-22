b4fc91834c5b8627ef4d787fac8fa5f88ad1bab2
sam.loose <sloose@jaguar.csuchico.edu>
2018-10-22 16:14:35 -0700

    finished first part

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include "Bitmap/Bitmap.h"
+#include "Bitmap/bitmap.h"
 #include <iostream>
 #include <vector>
 
@@ -8,6 +8,25 @@ using namespace std;
 //argv - each argument word, stored in an array
 int main(int argc, char* argv[])
 {
-  
+  Bitmap image;
+  string filename;
+  if(argc!=2)
+  {
+    cout<<"Please specify one image file!\n";
+  }
+  else
+  {
+    filename = argv[1];
+    image.open(filename);
+    if(image.isImage())
+    {
+        vector <vector <Pixel> > imagePixels = image.toPixelMatrix();
+
+    }
+    else
+    {
+        cout<<"Image file must be a bitmap with 24-bit color depth.\n";
+    }
+  }
   return 0;
 }
